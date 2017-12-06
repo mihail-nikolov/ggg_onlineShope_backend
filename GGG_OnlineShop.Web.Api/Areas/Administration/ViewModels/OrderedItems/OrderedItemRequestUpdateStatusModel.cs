1258d1011e9c930ccefa3b75b70352f0acaea421
mihail-nikolov <mihail.y.nikolov@abv.bg>
2017-12-06 11:02:40 +0200

    cleaned removed files

--- GGG_OnlineShop.Web.Api/Areas/Administration/ViewModels/OrderedItems/OrderedItemRequestUpdateStatusModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GGG_OnlineShop.Web.Api.Areas.Administration.ViewModels.OrderedItems
-{
-    public class OrderedItemRequestUpdateStatusModel
-    {
-        public int Id { get; set; }
-
-        public bool Finished { get; set; }
-    }
-}
--- GGG_OnlineShop.Web.Api/Models/AccountRequestForgottenPasswordCodeModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GGG_OnlineShop.Web.Api.Models
-{
-    public class AccountRequestForgottenPasswordCodeModel
-    {
-        public string Email { get; set; }
-    }
-}
